05bb8639754b3346e32e575f10ecafb2d46061cc
astaldo <astaldo@yeah.net>
2019-01-31 17:41:23 +0800

    增加中国DNS及相关网站测速，将美国网站和中国网站测速拆分为两个测速

--- dnstest.sh
+++ dnstest.sh
@@ -20,44 +20,72 @@ PROVIDERS="
 176.103.130.132#adguard 
 156.154.70.3#neustar 
 8.26.56.26#comodo
+114.114.114.114#114DNS-cn
+223.5.5.5#AliDNS-cn
+180.76.76.76#BaiduDNS-cn
+223.87.253.182#ChengduDNS-cn
+119.29.29.29#DNSPod-cn
+101.226.4.6#DNSPi-cn
 "
 
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+DOMAINS2TEST="
+www.google.com#google
+www.amazon.com#amazon
+www.facebook.com#facebook
+www.youtube.com#youtube
+www.twitter.com#twitter
+www.github.com#github
+www.gmail.com#gmail
+"
 
-
-totaldomains=0
-printf "%-18s" ""
-for d in $DOMAINS2TEST; do
-    totaldomains=$((totaldomains + 1))
-    printf "%-8s" "test$totaldomains"
-done
-printf "%-8s" "Average"
-echo ""
-
-
-for p in $NAMESERVERS $PROVIDERS; do
-    pip=${p%%#*}
-    pname=${p##*#}
-    ftime=0
-
-    printf "%-18s" "$pname"
-    for d in $DOMAINS2TEST; do
-        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
-        if [ -z "$ttime" ]; then
-	        #let's have time out be 1s = 1000ms
-	        ttime=1000
-        elif [ "x$ttime" = "x0" ]; then
-	        ttime=1
-	    fi
-
-        printf "%-8s" "$ttime ms"
-        ftime=$((ftime + ttime))
+DOMAINSCN2TEST="
+www.github.com#github
+www.baidu.com#baidu
+www.jd.com#jd
+www.taobao.com#taobao
+"
+function test()
+{
+    echo $1
+    totaldomains=0
+    printf "%-18s" ""
+    for d in $2; do
+        totaldomains=$((totaldomains + 1))
+        printf "%-10s" ${d##*#}
     done
-    avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
+    printf "%-11s" "Average"
+    printf "%-15s" "IP"
+    echo ""
 
-    echo "  $avg"
-done
+    for p in $NAMESERVERS $PROVIDERS; do
+        pip=${p%%#*}
+        pname=${p##*#}
+        ftime=0
 
+        printf "%-18s" "$pname"
+        for d in $2; do
+            ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+            if [ -z "$ttime" ]; then
+	            #let's have time out be 1s = 1000ms
+	            ttime=1000
+            elif [ "x$ttime" = "x0" ]; then
+	            ttime=1
+	        fi
+
+            printf "%-10s" "$ttime ms"
+            ftime=$((ftime + ttime))
+        done
+        avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
+
+        printf "%-11s" "$avg ms"
+        printf "%-15s" "$pip"
+        echo ""
+    
+    done
+}
+
+test "US Test" "$DOMAINS2TEST"
+test "CN Test" "$DOMAINSCN2TEST"
 
 exit 0;
